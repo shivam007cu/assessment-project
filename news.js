af1808f199e4c88c093bbc77ad69d2010f16a571
shivam007cu <44227531+shivam007cu@users.noreply.github.com>
2019-11-23 11:01:40 +0530

    Update news.js

--- news.js
+++ news.js
@@ -24,14 +24,8 @@ function getNews()
                 table=table+ '<div class="card-body" > <marquee><h5 class="card-title" style="text-shadow: 2px 2px #ff0000;font-size:40px;">'+data.news[i].title+'</h5></marquee>';
                 table=table+ '<p class="card-text" style="font-weight:bold">'+data.news[i].description+' </p>';
                 table=table+ '<p class="card-text"> <small class="text-muted" style="color:white"><p >'+data.news[i].author+'</p><p >'+data.news[i].published+'</p></small></p> </div> </div> </div>';
-                // table=table+"<tr>";
-                // table=table+"<td>"+(i+1)+"</td>";
-                // table=table+"<td>"+data.news[i].title+"</td>";
-                // table=table+"<td>"+data.news[i].author+"</td>";
-                // table=table+"<td>"+data.news[i].published+"</td>";
-                // table=table+"<td>"+data.news[i].description+"</td>";
-                // table=table+"</tr>";
-            }
+               
+           }
 
             $("#output-news").html(table);
          
